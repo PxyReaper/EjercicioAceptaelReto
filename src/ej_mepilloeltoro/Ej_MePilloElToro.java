4f8bf0e4f084a4c13bf48b1f2dff3aeddb1abc37
migue <migue@DESKTOP-1IFTLI5>
2024-03-17 00:00:46 +0100

    Acepta el reto Solucion

--- src/ej_mepilloeltoro/Ej_MePilloElToro.java
+++ src/ej_mepilloeltoro/Ej_MePilloElToro.java
@@ -18,47 +18,66 @@ public class Ej_MePilloElToro {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws FileNotFoundException {
-      File archivo = new File("src/ej_mepilloeltoro/datos");
-      Scanner entrada =  new Scanner(archivo);
-      int numero = 0;
-      String[] autor;
-      String comprobacion;
-      int[] evaluacion;
-      
-       numero = entrada.nextInt();
-       entrada.nextLine();
+        File archivo = new File("src/ej_mepilloeltoro/datos");
+        Scanner entrada = new Scanner(archivo);
+        int numero = 0;
+        String[] autor;
+        String comprobacion;
+        int[] evaluacion;
+
+        numero = entrada.nextInt();
+        entrada.nextLine();
         while (numero != 0)
         {
-        
+
             autor = new String[numero];
-            evaluacion =  new int[numero];
+            evaluacion = new int[numero];
+
             for (int i = 0; i < autor.length; i++)
             {
+
                 autor[i] = entrada.nextLine();
                 comprobacion = entrada.nextLine();
+
                 if (comprobacion.equals("CORRECTO"))
                 {
-                    evaluacion[i] ++;
-                }else{
-                    evaluacion[i] --;
-                
+                    evaluacion[i]++;
+                } else
+                {
+                    evaluacion[i]--;
+
                 }
+                if (i != 0 && autor[i].equals(autor[i - 1]))
+                {
+                    evaluacion[i - 1] += evaluacion[i];
+                }
+
             }
             mostrarAutor(autor, evaluacion);
-        
-            numero = entrada.nextInt();
 
-           
-          
+            numero = Integer.parseInt(entrada.nextLine());
+
         }
     }
-    public static void mostrarAutor(String []arrayAutores, int [] arrayEvaluacion ){
+
+    public static void mostrarAutor(String[] arrayAutores, int[] arrayEvaluacion) {
         for (int i = 0; i < arrayAutores.length; i++)
         {
-            if (arrayEvaluacion[i] != 0)
+            if (i == 0)
             {
-                System.out.println(arrayAutores[i] +"," + " " + arrayEvaluacion[i]);
+                 if (arrayEvaluacion[i] != 0 )
+            {
+                System.out.println(arrayAutores[i] + "," + " " + arrayEvaluacion[i]);
             }
+            }else{
+            
+                if (arrayEvaluacion[i] != 0 &&  !arrayAutores[i].equals(arrayAutores[i-1]))
+                {
+                    System.out.println(arrayAutores[i] + "," + " " +arrayEvaluacion[i]);
+                }
+            
+            }
+           
         }
         System.out.println("---");
     }
